bf7424bd3e034078d11785c75eb47575fae4588a
liaoxinjian <494461835@qq.com>
2023-07-03 18:43:48 +0800

    feature/exploit/modify: 变成ts的项目，还是有点问题

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 #! /usr/bin/env node
 const { Command } = require('commander');
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
 const fs = require('fs');
 const path = require('path');
 const { setTemplate } = require('./src/create');
 const { QUESTIONGROUP } = require('./src/enum');
 const program = new Command();
-
-interface CmdOptions {
-  template: string;
-}
 
 // 读取 package.json 配置
 const PACKAGE = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), 'package.json')));
@@ -25,12 +21,12 @@ program
   // 这里给选项一个必填值，如果给个可选值的话  到时候用户只输入 -t 没有带后面的参数，还得对这个结果进行处理
   // 用户不输入参数后，返回过来的参数结果为 { template: true }
   .option('-t, --template <value>', 'This is the selection template', 'default')
-  .action(async (name:string, cmd: CmdOptions) => {
+  .action(async (name, cmd) => {
     // 这是拿到了模板名字， 等下拿这个名字在用户目录下面通过 fs 模块写入文件名
-    // console.log('name is', name);
-    // console.log('cmd is', cmd);
-    const answer = await inquirer.prompt(QUESTIONGROUP);
-    // console.log(answer);
+    console.log('name is', name);
+    console.log('cmd is', cmd);
+    let answer = await inquirer.prompt(QUESTIONGROUP)
+    console.log(answer);
     setTemplate(cmd.template);
   });
 program.parse();
