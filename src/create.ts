813e06cc52e9682611809e4c586ed1bbad7a472e
liaoxinjian <494461835@qq.com>
2023-07-14 15:04:07 +0800

    fix: fix eslint and prettier

--- src/create.ts
+++ src/create.ts
@@ -3,24 +3,24 @@ import download from 'download-git-repo';
 import { TEMPLATE_GROUP, ERROR, DowloadError } from './enum.js';
 
 export default (data: string) => {
-    if (data.indexOf('/') > -1) {
-        // 这是对传入地址的处理
-        const reg = /http:\/\/|https:\/\//;
-        let templateUrl = '';
-        if (reg.test(data)) {
-            templateUrl = data;
-        } else {
-            templateUrl =
-                data[0] === '/'
-                    ? `https://github.com${data}`
-                    : `https://github.com/${data}`;
-        }
-        getTemplate(templateUrl);
+  if (data.indexOf('/') > -1) {
+    // 这是对传入地址的处理
+    const reg = /http:\/\/|https:\/\//;
+    let templateUrl = '';
+    if (reg.test(data)) {
+      templateUrl = data;
     } else {
-        if (!TEMPLATE_GROUP[data as keyof typeof TEMPLATE_GROUP])
-            throw new Error(ERROR.TEMPLAT_EERROR);
-        getTemplate(TEMPLATE_GROUP[data as keyof typeof TEMPLATE_GROUP]);
+      templateUrl =
+        data[0] === '/'
+          ? `https://github.com${data}`
+          : `https://github.com/${data}`;
     }
+    getTemplate(templateUrl);
+  } else {
+    if (!TEMPLATE_GROUP[data as keyof typeof TEMPLATE_GROUP])
+      throw new Error(ERROR.TEMPLAT_EERROR);
+    getTemplate(TEMPLATE_GROUP[data as keyof typeof TEMPLATE_GROUP]);
+  }
 };
 
 /**
@@ -28,10 +28,10 @@ export default (data: string) => {
  * @param {string} template 模板路径
  */
 const getTemplate = (template: string) => {
-    console.log('template is', template);
-    download(`direct:${template}`, 'test/tmp', (err: NodeJS.ErrnoException) => {
-        console.log(err ? 'error' : 'sucess');
-        console.log('err is', err);
-        if (err) console.log(`你所下载的模板${err.code}了，请重新检查下哈`);
-    });
+  console.log('template is', template);
+  download(`direct:${template}`, 'test/tmp', (err: NodeJS.ErrnoException) => {
+    console.log(err ? 'error' : 'sucess');
+    console.log('err is', err);
+    if (err) console.log(`你所下载的模板${err.code}了，请重新检查下哈`);
+  });
 };
