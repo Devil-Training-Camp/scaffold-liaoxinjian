dd8b527b575060cb70d4ee3589b14fd07fa1986f
liaoxinjian <494461835@qq.com>
2023-08-11 15:38:29 +0800

    feat: 优化 prettier 配置以及 index.ts的目录位置

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ import inquirer from 'inquirer';
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import setTemplate from './src/create.js';
-import { CmdOptions, QUESTION_GROUP } from './src/enum.js';
+import setTemplate from './create.ts';
+import { CmdOptions, QUESTION_GROUP } from './enum.ts';
 const program = new Command();
 
 // 为了兼容在 esm 中没有内置的 __dirname, 这里模拟了一个 __dirname
@@ -16,9 +16,7 @@ const packagePath = path.resolve(__dirnameNew, '../package.json');
 const PACKAGE = JSON.parse(fs.readFileSync(packagePath).toString());
 const VERSION = PACKAGE.version;
 
-program
-  .version(VERSION)
-  .option('-v, --version', 'This is the version');
+program.version(VERSION).option('-v, --version', 'This is the version');
 
 program
   .command('create <name>')
@@ -26,7 +24,7 @@ program
   // 这里给选项一个必填值，如果给个可选值的话  到时候用户只输入 -t 没有带后面的参数，还得对这个结果进行处理
   // 用户不输入参数后，返回过来的参数结果为 { template: true }
   .option('-t, --template <value>', 'This is the selection template', 'default')
-  .action(async (name:string, cmd: CmdOptions) => {
+  .action(async (name: string, cmd: CmdOptions) => {
     // console.log('name is', name);
     const answer = await inquirer.prompt(QUESTION_GROUP);
     // console.log(answer);
